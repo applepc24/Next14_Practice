fcb7b2fa48bab23a1aad5742951d9e7c526382a7
원준석 <wonjunseog@wonjunseog-ui-MacBookAir.local>
2023-12-01 18:35:52 +0900

    componet 연습

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,21 +1,24 @@
+import age from "./data.js";
+import hi from "./hello.js";
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <CartItem/>
+      <CartItem />
+      <CartItem />
+      <CartItem />
     </div>
   );
 }
+// 'use client' 는 유저 컴포넌트(자바스크립트 기능넣기 가능, 로딩속도 느림,hydration필요)
 
+//서버 컴포넌트 (자바스크립트 기능 넣기 불가능,로딩속도 빠름, 검색엔진 노출 유리)
 function CartItem() {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      {hi}
+      <p>상품명{age}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,15 +1,20 @@
-
 export default function List() {
-  let 상품 = ["맥북 에어", "맥북 프로", "맥 스튜디오"];
+  let 상품 = [
+    { name: "토마토", price: "₩1,5000,000" },
+    { name: "파스타", price: "₩1,400.000" },
+    { name: "코코넛", price: "₩1,3000,000" },
+  ];
 
   return (
     <div>
-      <h4 className="title">상품목록</h4>
-      {상품.map((product,i) => {
+      5<h4 className="title">상품목록</h4>
+      {상품.map((product, i) => {
         return (
           <div className="food" key={i}>
             <img src={`food${i}.png`} className="food-img" />
-            <h4>{product} ₩1,300,000</h4>
+            <h4>
+              {product.name} {product.price}
+            </h4>
           </div>
         );
       })}
