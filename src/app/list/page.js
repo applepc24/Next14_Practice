a83460d801f5bd44146ba76ff9c2c8a2da7e7287
원준석 <wonjunseog@wonjunseog-ui-MacBookAir.local>
2023-12-01 17:58:04 +0900

    image 넣기

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,12 +1,14 @@
+
 export default function List() {
   let 상품 = ["맥북 에어", "맥북 프로", "맥 스튜디오"];
 
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((product) => {
+      {상품.map((product,i) => {
         return (
-          <div className="food">
+          <div className="food" key={i}>
+            <img src={`food${i}.png`} className="food-img" />
             <h4>{product} ₩1,300,000</h4>
           </div>
         );
